209a21f8f58d26dd2cacee187c207b6383796471
Liz Wait <Liz.Wait@vmlyr.com>
2022-06-25 12:39:00 -0500

    Initial Commit

---
+++ grouplingios/grouplingiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  grouplingiosApp.swift
+//  grouplingios
+//
+//  Created by Liz Wait on 6/25/22.
+//
+
+import SwiftUI
+
+@main
+struct grouplingiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
