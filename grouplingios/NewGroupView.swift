a238b3da8e719fb98b08bd4a2922b68535b5a3c7
Liz Wait <Liz.Wait@vmlyr.com>
2022-07-03 15:53:08 -0500

    Added all sections to NewGroupView, added MapKit import

--- grouplingios/NewGroupView.swift
+++ grouplingios/NewGroupView.swift
@@ -6,76 +6,197 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct NewGroupView: View {
-    @State private var pinInput = ""
+    @State private var groupTitle = ""
+    @State private var groupDescription = ""
+    @State private var toggle = false
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var memberPhone = ""
+    @State private var memberEmail = ""
+    @State private var memberNotes = ""
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 39.099724, longitude: -94.578331), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+    @State private var searchPlaces = ""
+    @State private var groupNotes = ""
+    
     var body: some View {
-        ZStack {
-            Color.theme.background
-                .ignoresSafeArea()
-            VStack {
-                Image.image.grouplinglogo
-                    .resizable()
-                    .frame(width: 150, height: 75)
+        ScrollView {
+            ZStack {
+                Color.theme.background
+                    .ignoresSafeArea()
+                
                 VStack {
-                    ZStack {
-                        Rectangle()
-                            .fill(Color.theme.secondarybackground)
-                            .frame(width: 400, height: 350)
-                        VStack {
-                            Text("ddd")
-                            TextField("Text Input", text: $pinInput)
-                                .background(Color.theme.bright)
-                                .padding(.top, 40)
-                                .padding(.trailing, 120)
-                                .padding(.leading, 120)
-                                .textFieldStyle(.roundedBorder)
-                                .cornerRadius(6)
-                            Button {}
-                        label: {
-                            Text("add")
-                                .padding(10)
-                                .background(Color.theme.accent)
-                                .font(.system(size: 15, weight: .semibold, design: .rounded))
-                                .foregroundColor(Color.theme.bright)
-                                .cornerRadius(6)
-                                .padding(.top, 10)
+                    HStack {
+                        Image.image.grouplinglogo
+                            .resizable()
+                            .frame(width: 200, height: 75)
+                            .padding(.leading, 5)
+                        
+                        Toggle (isOn: $toggle) {
+                            HStack {
+                                Spacer()
+                                Image.image.mulleticon
+                                    .resizable()
+                                    .frame(width: 40, height: 40)
+                            }
                         }
-                        }
+                        .padding(.trailing, 25)
                     }
-                }
-                VStack {
-                    ZStack {
-                        Rectangle()
-                            .fill(Color.theme.secondarybackground)
-                            .frame(width: 400, height: 350)
+                    
+                    VStack {
+                        ZStack {
+                            Rectangle()
+                                .fill(Color.theme.secondarybackground)
+                                .frame(height: 350)
+                            VStack {
+                                Text("Group Title")
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .font(.system(size: 20, weight: .bold, design: .rounded))
+                                    .padding()
+                                TextField("", text: $groupTitle)
+                                    .background()
+                                    .padding(.trailing, 20)
+                                    .padding(.leading, 20)
+                                Text("Group Description")
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .font(.system(size: 20, weight: .bold, design: .rounded))
+                                    .padding()
+                                TextEditor(text: $groupDescription)
+                                    .background()
+                                    .padding(.trailing, 20)
+                                    .padding(.leading, 20)
+                                    .padding(.bottom)
+                            }
+                            .textFieldStyle(.roundedBorder)
+                            .cornerRadius(6)
+                            .font(.system(size: 15, weight: .semibold, design: .rounded))
+                        }
+                        Spacer()
+                        
                         VStack {
-                            TextField("Text Input", text: $pinInput)
-                                .background(Color.theme.bright)
-                                .padding(.top, 40)
-                                .padding(.trailing, 120)
-                                .padding(.leading, 120)
-                                .textFieldStyle(.roundedBorder)
-                                .cornerRadius(6)
-                            Button {}
+                            ZStack {
+                                Rectangle()
+                                    .fill(Color.theme.secondarybackground)
+                                    .frame(height: 600)
+                                VStack {
+                                    Text("Members")
+                                        .frame(maxWidth: .infinity, alignment: .center)
+                                        .font(.system(size: 20, weight: .bold, design: .rounded))
+                                        .padding()
+                                    TextField("First Name", text: $firstName)
+                                        .background()
+                                    TextField("Last Name", text: $lastName)
+                                        .background()
+                                    TextField("Phone Number", text: $memberPhone)
+                                        .background()
+                                    TextField("Email", text: $memberEmail)
+                                        .background()
+                                    Text("Member Specific Notes")
+                                        .frame(maxWidth: .infinity, alignment: .leading)
+                                        .padding(.top, 15)
+                                        .font(.system(size: 18, weight: .semibold, design: .rounded))
+                                    TextEditor(text: $groupDescription)
+                                        .background()
+                                        .padding(.bottom)
+                                        .textFieldStyle(.roundedBorder)
+                                        .cornerRadius(6)
+                                    Button {}
                                 label: {
-                                    Text("add")
+                                    Text("add member to group")
                                         .padding(10)
                                         .background(Color.theme.accent)
                                         .font(.system(size: 15, weight: .semibold, design: .rounded))
                                         .foregroundColor(Color.theme.bright)
                                         .cornerRadius(6)
                                         .padding(.top, 10)
+                                        .padding(.bottom)
                                 }
+                                    Divider()
+                                    Text("Member List")
+                                        .frame(maxWidth: .infinity, alignment: .leading)
+                                        .font(.system(size: 20, weight: .bold, design: .rounded))
+                                        .padding()
+                                }
+                                .padding(.trailing, 20)
+                                .padding(.leading, 20)
+                                .textFieldStyle(.roundedBorder)
+                                .cornerRadius(6)
+                                .font(.system(size: 15, weight: .semibold, design: .rounded))
+                            }
                         }
+                        Spacer()
+                        ZStack {
+                            Rectangle()
+                                .fill(Color.theme.secondarybackground)
+                                .frame(height:480)
+                            VStack {
+                                Text("Places")
+                                    .frame(maxWidth: .infinity, alignment: .center)
+                                    .font(.system(size: 20, weight: .bold, design: .rounded))
+                                    .padding()
+                                TextField("Search Places", text: $searchPlaces)
+                                    .background()
+                                Map(coordinateRegion: $region)
+                                    .frame(width: 400, height: 300)
+                                Divider()
+                                Text("Places List")
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .font(.system(size: 20, weight: .bold, design: .rounded))
+                                    .padding()
+                            }
+                            .padding(.trailing, 20)
+                            .padding(.leading, 20)
+                            .textFieldStyle(.roundedBorder)
+                            .cornerRadius(6)
+                            .font(.system(size: 15, weight: .semibold, design: .rounded))
+                        }
+                        Spacer()
+                        ZStack {
+                            Rectangle()
+                                .fill(Color.theme.secondarybackground)
+                                .frame(height:480)
+                            VStack {
+                                Text("Notes")
+                                    .frame(maxWidth: .infinity, alignment: .center)
+                                    .font(.system(size: 20, weight: .bold, design: .rounded))
+                                    .padding()
+                                TextEditor(text: $groupNotes)
+                                    .background()
+                                    .padding(.bottom)
+                                Button {}
+                            label: {
+                                Text("add note")
+                                    .padding(10)
+                                    .background(Color.theme.accent)
+                                    .font(.system(size: 15, weight: .semibold, design: .rounded))
+                                    .foregroundColor(Color.theme.bright)
+                                    .cornerRadius(6)
+                                    .padding(.top, 10)
+                                    .padding(.bottom)
+                            }
+                                Divider()
+                                Text("Group Notes")
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .font(.system(size: 20, weight: .bold, design: .rounded))
+                                    .padding()
+                            }
+                            .padding(.trailing, 20)
+                            .padding(.leading, 20)
+                            .textFieldStyle(.roundedBorder)
+                            .cornerRadius(6)
+                            .font(.system(size: 15, weight: .semibold, design: .rounded))
+                        }
+                        Spacer()
                     }
                 }
             }
         }
     }
-}
-struct NewGroupView_Previews: PreviewProvider {
-    static var previews: some View {
-        NewGroupView()
+    struct NewGroupView_Previews: PreviewProvider {
+        static var previews: some View {
+            NewGroupView()
+        }
     }
 }
