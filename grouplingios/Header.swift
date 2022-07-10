0000330d3ed8a4200eb4ca7273003b23ca5109f4
Liz Wait <Liz.Wait@vmlyr.com>
2022-07-10 15:57:37 -0500

    working on toggle to switch themes

--- grouplingios/Header.swift
+++ grouplingios/Header.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct Header: View {
     
+    @StateObject var themeManager = ThemeManager()
     @State private var toggle = false
     
     var body: some View {
@@ -17,18 +18,26 @@ struct Header: View {
                 .resizable()
                 .frame(width: 200, height: 75)
             
-            Toggle (isOn: $toggle) {
-                HStack {
-                    Spacer()
-                    Image.image.mulleticon
-                        .resizable()
-                        .frame(width: 40, height: 40)
+            Toggle ("", isOn: $toggle)
+//                ForEach(0..<themeManager.themes.count)
+                .onChange(of: toggle) { value in
+                    if value == true {
+                        themeManager.applyTheme(1)
+                    } else {
+                        themeManager.applyTheme(0)
+                    }
                 }
+            HStack {
+                Spacer()
+                Image.image.mulleticon
+                    .resizable()
+                    .frame(width: 40, height: 40)
             }
         }
-        .padding([.trailing, .leading], 30)
     }
+    //    .padding([.trailing, .leading], 30)
 }
+
 
 struct Header_Previews: PreviewProvider {
     static var previews: some View {
